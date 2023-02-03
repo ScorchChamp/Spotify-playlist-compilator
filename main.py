8a573a9edb07075aaf327a3aa74538f15fbd6a83
Lars <l.cornelissen5@student.han.nl>
2023-02-03 13:40:57 +0100

    src

--- main.py
+++ main.py
@@ -16,24 +16,23 @@ def run(playlistID, channelID):
     LIST_FILE = f'{BASE_DIR}/{playlistID}.txt'
     pData = sapi.getPlaylistData(playlistID)
 
-    # videoPaths = [ ]
-    # index = 0
+    videoPaths = [ ]
+    index = 0
     items = sapi.getPlaylistItems(playlistID)
-    # for item in items:
-    #     index += 1
-    #     url = yapi.downloadVideo(item)
-    #     videoLength = fw.getVideoLength(url)
-    #     video = fw.cutVideo(url, videoLength/2, SNIPPET_LENGTH if index != 1 else SNIPPET_LENGTH*2, FADE_LENGTH)
-    #     video = fw.addTextOverlay(video, [(f"{index}. {item.title}", 100, 150, 64, 'Roboto-Regular.ttf'), (', '.join(item.artists), 100, 75, 52, 'Roboto-Thin.ttf')], videoLength, FADE_LENGTH)
-    #     videoPaths.append(video)
+    for item in items:
+        index += 1
+        url = yapi.downloadVideo(item)
+        videoLength = fw.getVideoLength(url)
+        video = fw.cutVideo(url, videoLength/2, SNIPPET_LENGTH if index != 1 else SNIPPET_LENGTH*2, FADE_LENGTH)
+        video = fw.addTextOverlay(video, [(f"{index}. {item.title}", 100, 150, 64, 'Roboto-Regular.ttf'), (', '.join(item.artists), 100, 75, 52, 'Roboto-Thin.ttf')], videoLength, FADE_LENGTH)
+        videoPaths.append(video)
 
-    # videoPaths.reverse()
-    # with open(LIST_FILE, 'w') as f:
-    #     for file in videoPaths:
-    #         f.write(f"""file 'file:{file}'\n""")
+    videoPaths.reverse()
+    with open(LIST_FILE, 'w') as f:
+        for file in videoPaths:
+            f.write(f"""file 'file:{file}'\n""")
 
-    # resultFile = fw.concatVideos(LIST_FILE, f'{playlistID}.mp4')
-    resultFile = 'E:\ScorchLLC\Spotify-playlist-compilator/videos/37i9dQZF1EQn4jwNIohw50.mp4'
+    resultFile = fw.concatVideos(LIST_FILE, f'{playlistID}.mp4')
     print(resultFile)
     print(pData['description'], pData['name'], pData['images'])
 
